294c3f93433301aad446f1cd99893eb5ed1557a9
Fylls <sergentifilippo@gmail.com>
2020-05-17 09:49:07 +0200

    insert

---
+++ CRUD Operations/1 - Create/import/importing.js
@@ -0,0 +1,13 @@
+// mongoimport [path] -d [movieDatabase] -c [movies]  --jsonArray
+
+/*
+
+path to eh file,
+
+which database?
+
+which collectin inside the selected database?
+
+what type of document is it?
+
+*/
